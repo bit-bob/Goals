dfa4fc557f29e97b36a73a7c0b7ecfde6ffa4d96
Helina <helina.berhane@outlook.com>
2024-01-31 16:38:05 +0000

    GoalsTable - add a progress bar

--- frontend/src/Goals/GoalsTable.tsx
+++ frontend/src/Goals/GoalsTable.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Table } from "@mantine/core";
+import { DefaultMantineColor, Progress, Table } from "@mantine/core";
 import { Goal } from "api-client";
 
 export interface GoalsTableProps {
@@ -8,15 +8,26 @@ export interface GoalsTableProps {
 }
 
 export function GoalsTable({ goals }: GoalsTableProps) {
-  const rows = goals.map((goal) => (
-    <Table.Tr key={goal.id}>
-      <Table.Td>{goal.name}</Table.Td>
-      <Table.Td>{goal.unit}</Table.Td>
-      <Table.Td>{goal.intervalTargetAmount}</Table.Td>
-      <Table.Td>{goal.progress}</Table.Td>
-      <Table.Td>{goal.goalProgress}</Table.Td>
-    </Table.Tr>
-  ));
+  const rows = goals.map((goal) => {
+    const progressPercent = 100 * (goal.progress ?? 0 - goal.intervalStartAmount) / (goal.goalProgress ?? goal.intervalTargetAmount - goal.intervalStartAmount);
+    const progressColour : DefaultMantineColor = progressPercent >= 100 ? "green" : "blue";
+    return (
+      <Table.Tr key={goal.id}>
+        <Table.Td>{goal.name}</Table.Td>
+        <Table.Td>{goal.unit}</Table.Td>
+        <Table.Td>{goal.intervalTargetAmount}</Table.Td>
+        <Table.Td>{goal.progress}</Table.Td>
+        <Table.Td>{goal.goalProgress}</Table.Td>
+        <Table.Td><Progress
+          radius="xs"
+          size="xl"
+          color={progressColour}
+          value={progressPercent}
+          style={{ width: 100 }}
+        /></Table.Td>
+      </Table.Tr>
+    )
+  });
 
   return (
     <Table>
